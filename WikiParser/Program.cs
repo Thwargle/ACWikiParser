af798f55e1e7f492c1442ad80facc5b9c2965f88
Zorgle <zorgle@users.noreply.github.com>
2017-07-12 23:50:46 -0400

    Add VS2012 solution and project; Check for file presence at start

--- WikiParser/Program.cs
+++ WikiParser/Program.cs
@@ -7,7 +7,22 @@ namespace WikiParser
     {
         static void Main(string[] args)
         {
-            using (StreamReader reader = File.OpenText("asheron_backup2_pages_current.xml"))
+            const string filename = "asheron_backup2_pages_current.xml";
+            CheckFile(filename);
+            ParseAndOutputFile(filename);
+        }
+        private static void CheckFile(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                return;
+            }
+//            const string url = "http://s3.amazonaws.com/wikia_xml_dumps/a/as/asheron_backup2_pages_current.xml.7z";
+            throw new Exception("File not found: " + filename);
+        }
+        private static void ParseAndOutputFile(string filename)
+        {
+            using (StreamReader reader = File.OpenText(filename))
             {
                 Parser parser = new Parser();
                 parser.Parse(reader);
