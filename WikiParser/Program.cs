3de80751ba3411188051ebb659f98c18645ae702
Zorgle <zorgle@users.noreply.github.com>
2017-07-13 00:48:55 -0400

    Add code to download data file and unzip

--- WikiParser/Program.cs
+++ WikiParser/Program.cs
@@ -13,12 +13,80 @@ namespace WikiParser
         }
         private static void CheckFile(string filename)
         {
-            if (File.Exists(filename))
+            if (!File.Exists(filename))
             {
-                return;
+                DownloadFile(filename);
             }
-//            const string url = "http://s3.amazonaws.com/wikia_xml_dumps/a/as/asheron_backup2_pages_current.xml.7z";
-            throw new Exception("File not found: " + filename);
+            if (!File.Exists(filename))
+            {
+                throw new Exception("File not found: " + filename);
+            }
+        }
+        private static void DownloadFile(string filename)
+        {
+            const string url = "http://s3.amazonaws.com/wikia_xml_dumps/a/as/asheron_backup2_pages_current.xml.7z";
+            const string localzipfile = "asheron_backup2_pages_current.xml.7z";
+            using (var client = new System.Net.WebClient())
+            {
+                Log("Downloading file: " + url);
+                client.DownloadFile(url, localzipfile);
+            }
+            Log("Extracting file");
+            ExtractFile(localzipfile, filename);
+        }
+        private static void Log(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        private static string Find7zipExe()
+        {
+            string dirpath = GetHklmValue(@"SOFTWARE\7-Zip", "Path64", defaultValue: null);
+            if (dirpath == null)
+            {
+                dirpath = @"C:\Program Files\7-Zip\";
+            }
+            string filepath = System.IO.Path.Combine(dirpath, "7zG.exe");
+            if (File.Exists(filepath))
+            {
+                return filepath;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        private static string GetHklmValue(string key, string name, string defaultValue)
+        {
+            Microsoft.Win32.RegistryKey localKey;
+            if (Environment.Is64BitOperatingSystem)
+                localKey = Microsoft.Win32.RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, Microsoft.Win32.RegistryView.Registry64);
+            else
+                localKey = Microsoft.Win32.RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, Microsoft.Win32.RegistryView.Registry32);
+
+            object value = localKey.OpenSubKey(key).GetValue(name);
+            if (value != null) { return value.ToString(); }
+            else { return defaultValue; }
+        }
+        private static void ExtractFile(string source, string destination)
+        {
+            string zPath = Find7zipExe();
+            if (zPath == null)
+            {
+                throw new Exception("7Zip not found");
+            }
+            try
+            {
+                var pro = new System.Diagnostics.ProcessStartInfo();
+                pro.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                pro.FileName = zPath;
+                pro.Arguments = "e \"" + source + "\""; //+destination;
+                var x = System.Diagnostics.Process.Start(pro);
+                x.WaitForExit();
+            }
+            catch (System.Exception exc)
+            {
+                throw new Exception("Exception invoking 7Zip", exc);
+            }
         }
         private static void ParseAndOutputFile(string filename)
         {
