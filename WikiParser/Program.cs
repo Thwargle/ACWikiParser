99389e467b15622710973b567f49c0e0ba221b46
zorgle <zorgle@users.noreply.github.com>
2017-07-14 13:36:12 -0400

    Modify code to use HttpClient instead of WebClient, and modify VS2017 project to use .NET Framework so it can access registry

--- WikiParser/Program.cs
+++ WikiParser/Program.cs
@@ -26,13 +26,28 @@ namespace WikiParser
         {
             const string url = "http://s3.amazonaws.com/wikia_xml_dumps/a/as/asheron_backup2_pages_current.xml.7z";
             const string localzipfile = "asheron_backup2_pages_current.xml.7z";
-            using (var client = new System.Net.WebClient())
-            {
-                Log("Downloading file: " + url);
-                client.DownloadFile(url, localzipfile);
-            }
+            Log("Downloading file: " + url);
+            var task = ReadToFile(url, localzipfile);
+            task.Wait();
             Log("Extracting file");
             ExtractFile(localzipfile, filename);
+
+        }
+        private static async System.Threading.Tasks.Task ReadToFile(string url, string targetfile)
+        {
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                using (var response = await client.GetAsync(url, System.Net.Http.HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false))
+                {
+                    using (var httpStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                    {
+                        using (var targetStream = File.OpenWrite(targetfile))
+                        {
+                            await httpStream.CopyToAsync(targetStream).ConfigureAwait(false);
+                        }
+                    }
+                }
+            }
         }
         private static void Log(string msg)
         {
